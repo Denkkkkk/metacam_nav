7419510fc67d9e7fe8923a1d0faf6932901fa33c
ldq <1327165187@qq.com>
2024-12-24 19:38:23 +0800

    [amd64-denk]fixed bug

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-catkin_make install -DCMAKE_INSTALL_PREFIX=output/metacam_nav/opt/skyland/metacam_nav
+catkin_make install -DCMAKE_INSTALL_PREFIX=output/metacam_nav/opt/skyland/metacam_nav -DCMAKE_BUILD_TYPE=Release
 
