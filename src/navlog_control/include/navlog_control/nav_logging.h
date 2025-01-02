dbe0bede6d108abe61c03551e550387de3fc9d77
ldq <1327165187@qq.com>
2025-01-02 16:22:52 +0800

    --

--- src/navlog_control/include/navlog_control/nav_logging.h
+++ src/navlog_control/include/navlog_control/nav_logging.h
@@ -11,7 +11,7 @@
  *
  */
 
-#include <cstdio>
+#include <cstdio> // for printf
 #include <cstring>
 #include <ctime>
 #include <iostream>
@@ -33,9 +33,32 @@
 #define DEBUG_NAV_INFO(msg) NAV_INFO(msg)
 #define DEBUG_NAV_PASS(msg) NAV_PASS(msg)
 #endif
-void logWithColor(const char* color, const char* node_name, const char* timeStr, const char* msg);
-void NAV_HIGHLIGHT(const char* msg);
-void NAV_WARNING(const char* msg);
-void NAV_PASS(const char* msg);
-void NAV_ERROR(const char* msg);
-void NAV_INFO(const char* msg);
+// void logWithColor(const char* color, const char* node_name, const char* timeStr, const char* msg);
+// void NAV_HIGHLIGHT(const char* msg,...);
+// void NAV_WARNING(const char* msg,...);
+// void NAV_PASS(const char* msg,...);
+// void NAV_ERROR(const char* msg,...);
+// void NAV_INFO(const char* msg,...);
+
+// 35m 品红
+#define NAV_HIGHLIGHT(msg...)                                           \
+    do                                                                  \
+    {                                                                   \
+        /*获取当前时间 */                                               \
+        time_t rawtime;                                                 \
+        struct tm timeinfo;                                             \
+        char timeStr[20];                                               \
+        time(&rawtime);                                                 \
+        timeinfo = localtime(&rawtime);                                 \
+        strftime(timeStr, sizeof(timeStr), "%m-%d %H:%M:%S", timeinfo); \
+        /*获取当前 ROS 节点名称 */                                      \
+        const char node_name = ros::this_node::getName().c_str();       \
+        if (!node_name)                                                 \
+        {                                                               \
+            node_name = "Unknown_Node";                                 \
+        }                                                               \
+        /*打印日志消息 */                                               \
+        printf("\033[35m[%s] [%s] ", node_name, timeStr);               \
+        printf(msg);                                                    \
+        printf("\033[0m\n");                                            \
+    } while (false)
--- src/navlog_control/src/navlog_control.cpp
+++ src/navlog_control/src/navlog_control.cpp
@@ -6,10 +6,13 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     for (int i = 0; i < 10; i++)
     {
-        NAV_INFO("This is a ROS_INFO message %d", i);
-        NAV_WARN("This is a ROS_WARN message %s", "test");
-        NAV_ERROR("This is a ROS_ERROR message %f", i * 0.1);
-        NAV_HIGHLIGHT("This is a ROS_FATAL message %d", i);
+//        NAV_INFO("This is a NAV_INFO message %d", i);
+//        NAV_WARNING("This is a NAV_WARNING message %s", "test");
+//        NAV_ERROR("This is a NAV_ERROR message %f", i * 0.1);
+//        NAV_HIGHLIGHT("This is a NAV_HIGHLIGHT message %d", i);
+          NAV_HIGHLIGHT("This is a NAV_HIGHLIGHT message %d", 1);
     }
+    ROS_INFO( "Hello, world!" );
+
     return 0;
 }
